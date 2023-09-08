3eeb2cc77ba8bad79c9d16d9f195545b5c068a08
juandeaguero <juandeaguero@gmail.com>
2023-09-08 12:53:33 -0400

    no comment

--- Source/ElementWise.hpp
+++ Source/ElementWise.hpp
@@ -7,38 +7,6 @@
 
 namespace Flow
 {
-    /**
-     * @brief Provides an element-wise operation functionality on NArrays.
-     *
-     * @details The "ElementWise" function namespace performs element-wise operations 
-     * (addition, subtraction, multiplication, ...) on NArray objects. This implementation 
-     * supports operations on both 1D and 2D arrays.
-     *
-     * Shape verification:
-     * The function checks if the arrays are either 1D or 2D. It doesn't support arrays 
-     * with dimensions higher than 2.
-     *
-     * Reshaping for broadcasting:
-     * The function can reshape 1D arrays to align with 2D arrays for operations, enabling 
-     * broadcasting functionality similar to what is found in numpy.
-     * 
-     * Shape compatibility:
-     * Before performing operations, the function ensures that the two arrays have 
-     * compatible shapes. Two shapes are considered compatible if they are equal, or one 
-     * of them is a 1D shape that can be broadcasted to match the 2D shape of the other.
-     *
-     * Performing element-wise operation:
-     * The operation to be performed (addition, subtraction, multiplication, or division) is 
-     * determined by the `NArray::Operation` enumeration passed to the function. The operation 
-     * is applied element-wise, taking into account the broadcasting rules if necessary.
-     *
-     * @param arr1 Pointer to the first NArray object.
-     * @param arr2 Pointer to the second NArray object.
-     * @param op The element-wise operation to perform from NArray::Operation.
-     * 
-     * @return Pointer to the resulting NArray after performing the specified operation or 
-     * nullptr if the operation couldn't be performed due to shape incompatibility.
-     */
     NArray* ElementWise( NArray* arr1, NArray* arr2, NArray::Operation op )
     {
         if ( arr1->GetShape().size() > 2 ||  arr2->GetShape().size() > 2 )
